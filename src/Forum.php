4a97f58571d4e5f2abdec2a17489ff3bd35fcdfe
Ionut G. Stan <ionut.g.stan@gmail.com>
2010-03-31 12:51:10 +0300

    Add test and implementation for creating topics

--- src/Forum.php
+++ src/Forum.php
@@ -6,14 +6,22 @@ class Forum
 {
     private $topics;
 
+    private $dataStore;
 
-    public function __construct($topics)
+
+    public function __construct($topics, $dataStore)
     {
         $this->topics = $topics;
+        $this->dataStore = $dataStore;
     }
 
     public function getTopics()
     {
         return $this->topics;
+    }
+
+    public function createTopic($topicTitle, $firstPost)
+    {
+        $this->dataStore->saveTopic($topicTitle, $firstPost);
     }
 }
--- tests/ForumTest.php
+++ tests/ForumTest.php
@@ -13,8 +13,27 @@ class ForumTest extends PHPUnit_Framework_TestCase
     public function getTopis()
     {
         $topics = array('Topic #1', 'Topic #2');
-        $forum  = new Forum($topics);
+        $dataStore = new StdClass;
+        $forum = new Forum($topics, $dataStore);
 
         $this->assertEquals($topics, $forum->getTopics());
+    }
+
+    /**
+     * @test
+     */
+    public function createTopic()
+    {
+        $existingTopics = array();
+        $topicTitle = 'First topic';
+        $firstPost = 'First post';
+
+        $dataStore = $this->getMock('StdClass', array('saveTopic'));
+        $dataStore->expects($this->once())
+                  ->method('saveTopic')
+                  ->with($this->equalTo($topicTitle), $this->equalTo($firstPost));
+
+        $forum = new Forum($existingTopics, $dataStore);
+        $forum->createTopic($topicTitle, $firstPost);
     }
 }
