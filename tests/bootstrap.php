63bd9dd79f21f9180dfa7721776e893c55e831ed
Ionut G. Stan <ionut.g.stan@gmail.com>
2010-03-31 15:23:32 +0300

    Create bootstrap file for tests
    Needed in order to prepend values to include_path.

---
+++ tests/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @author  Ionut G. Stan
+ * @license http://opensource.org/licenses/bsd-license.php BSD License
+ */
+
+set_include_path(implode(PATH_SEPARATOR, array(
+    realpath(__DIR__ . '/../src'),
+    get_include_path(),
+)));
